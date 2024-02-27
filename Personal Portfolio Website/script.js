a662d0fe75c8bf7ad77fb91cd6b8c00985fd2c2a
Manideep-15 <138442108+Manideep-15@users.noreply.github.com>
2024-02-27 19:11:33 +0530

    Add files via upload

---
+++ Personal Portfolio Website/script.js
@@ -0,0 +1,36 @@
+// Header scrolling effect
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > 0) {
+      header.classList.add("nav-show");
+    } else {
+      header.classList.remove("nav-show");
+    }
+  });
+  
+  // Hamburger menu
+  const navSlide = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navbar = document.querySelector(".nav-bar");
+    const navLinks = document.querySelectorAll(".nav-bar li");
+  
+    hamburger.addEventListener("click", () => {
+      navbar.classList.toggle("nav-active");
+  
+      // Animation links
+      navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = "";
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${
+            (index / 7 + 1) * 0.2
+          }s`;
+        }
+      });
+  
+      // Hamburger animation
+      hamburger.classList.toggle("toggle");
+    });
+  };
+  
+  window.onload = () => navSlide();
